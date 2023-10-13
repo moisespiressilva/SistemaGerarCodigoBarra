0d6f6418c6d6517b1e6798ef96ecf060b820b4c1
Remoto <Remoto@REMOTO>
2023-10-13 11:53:05 -0300

    Inicial 1.0

--- SistemaGerarCodigoBarra/Form1.Designer.cs
+++ SistemaGerarCodigoBarra/Form1.Designer.cs
@@ -28,13 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnGerar = new System.Windows.Forms.Button();
+            this.btnImprimir = new System.Windows.Forms.Button();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.pictureCodigoBarra = new System.Windows.Forms.PictureBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCodigoBarra)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGerar
+            // 
+            this.btnGerar.Location = new System.Drawing.Point(197, 339);
+            this.btnGerar.Name = "btnGerar";
+            this.btnGerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGerar.TabIndex = 0;
+            this.btnGerar.Text = "Gerar";
+            this.btnGerar.UseVisualStyleBackColor = true;
+            this.btnGerar.Click += new System.EventHandler(this.btnGerar_Click);
+            // 
+            // btnImprimir
+            // 
+            this.btnImprimir.Location = new System.Drawing.Point(341, 339);
+            this.btnImprimir.Name = "btnImprimir";
+            this.btnImprimir.Size = new System.Drawing.Size(75, 23);
+            this.btnImprimir.TabIndex = 1;
+            this.btnImprimir.Text = "Imprimir";
+            this.btnImprimir.UseVisualStyleBackColor = true;
+            this.btnImprimir.Click += new System.EventHandler(this.btnImprimir_Click);
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.Location = new System.Drawing.Point(488, 339);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(75, 23);
+            this.btnSalvar.TabIndex = 2;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = true;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // printDocument1
+            // 
+            this.printDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(this.printDocument1_PrintPage);
+            // 
+            // pictureCodigoBarra
+            // 
+            this.pictureCodigoBarra.Location = new System.Drawing.Point(61, 232);
+            this.pictureCodigoBarra.Name = "pictureCodigoBarra";
+            this.pictureCodigoBarra.Size = new System.Drawing.Size(692, 50);
+            this.pictureCodigoBarra.TabIndex = 3;
+            this.pictureCodigoBarra.TabStop = false;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(236, 180);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(267, 20);
+            this.txtCodigo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(313, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Gerar código de Barras";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.pictureCodigoBarra);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.btnImprimir);
+            this.Controls.Add(this.btnGerar);
+            this.Name = "Form1";
+            this.Text = "Código de Barra";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCodigoBarra)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGerar;
+        private System.Windows.Forms.Button btnImprimir;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.PictureBox pictureCodigoBarra;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- SistemaGerarCodigoBarra/Form1.cs
+++ SistemaGerarCodigoBarra/Form1.cs
@@ -7,14 +7,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GenCode128;
 
 namespace SistemaGerarCodigoBarra
 {
     public partial class Form1 : Form
     {
+        //Define a altura do código de barras
+        public int altura = 2;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnGerar_Click(object sender, EventArgs e)
+        {
+            // Gera o código de barras de acordo com o texto digitado, na altura definida e TRUE PARA adicionar um espaço antes 
+            Image codigoBarras = Code128Rendering.MakeBarcodeImage(txtCodigo.Text, altura, true);
+            pictureCodigoBarra.Image = codigoBarras;
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            //Pega a picture e sava com o nome do código digitado e a extensão JPG
+            pictureCodigoBarra.Image.Save(txtCodigo.Text+".jpg");
+        }
+
+        private void btnImprimir_Click(object sender, EventArgs e)
+        {
+            this.printDocument1.Print();
+        }
+
+        private void printDocument1_PrintPage(object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            using (var g = e.Graphics)
+            {           // Definindo os parametros do código digitado para ser exibido
+                using (var fnt = new Font("Courier New", 16))
+                {
+                    g.DrawImage(this.pictureCodigoBarra.Image, 20, 50); // 20 Margem a esquerda e 50 margem superior
+
+                    var caption = txtCodigo.Text;
+                    // Exibir o código digitado na cor preta, com as coordenadas
+                    g.DrawString(caption, fnt, Brushes.Black, 130, 110);
+                }
+            }
         }
     }
 }
